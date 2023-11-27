8be6387fb5e354eeaaf4c97043c93c67182edded
rudrax14 <m.rudra2003@gmail.com>
2023-11-27 17:45:48 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
     <div>
       <main class="flex justify-center gap-4 flex-col min-h-screen">
         <h1 class="text-3xl text-center font-bold underline">
-          HTML & Tailwind CSS Starter Pack
+          React & Tailwind CSS Starter Pack
         </h1>
         <p class="text-center text-xl">
-          This is a starter pack for HTML & Tailwind CSS projects.
+          This is a starter pack for React & Tailwind CSS projects.
         </p>
         <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
       </main>
