1562e388979851a453ab3a1caaaef902836c7529
rudrax14 <m.rudra2003@gmail.com>
2023-11-27 17:45:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <main class="flex justify-center gap-4 flex-col min-h-screen">
+        <h1 class="text-3xl text-center font-bold underline">
+          HTML & Tailwind CSS Starter Pack
+        </h1>
+        <p class="text-center text-xl">
+          This is a starter pack for HTML & Tailwind CSS projects.
+        </p>
+        <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
+      </main>
+    </div>
+  );
+}
+
+export default App;
